d05642ed802f8ffb25f94c5eb0f7c2611e1b6adb
Chima Precious <ositachima.co@gmail.com>
2025-06-02 09:42:58 +0000

    chore: Update globe_runtime examples

--- examples/basic_example/lib/basic_example.dart
+++ examples/basic_example/lib/basic_example.dart
@@ -5,16 +5,18 @@ import 'dart:io';
 import 'package:globe_runtime/globe_runtime.dart';
 import 'package:path/path.dart' as path;
 
-const moduleName = 'BasicExample';
+final module = FileModule(
+  name: 'BasicExample',
+  filePath: path.join(Directory.current.path, 'lib', 'basic_example.js'),
+);
 
 Future<List<int>> callJsFunction(
   String functionName, {
   List<FFIConvertible> args = const [],
 }) async {
   final completer = Completer<List<int>>();
-  GlobeRuntime.instance.callFunction(
-    moduleName,
-    function: functionName,
+  module.callFunction(
+    functionName,
     args: args,
     onData: (data) {
       if (data.hasError()) {
@@ -29,20 +31,12 @@ Future<List<int>> callJsFunction(
 }
 
 void main() async {
-  final runtime = GlobeRuntime.instance;
-  final jsCodePath =
-      path.join(Directory.current.path, 'lib', 'basic_example.js');
-
-  await runtime.registerModule(
-    moduleName,
-    jsCodePath,
-    args: ['100'.toFFIType],
-  );
+  await module.register(args: ['100'.toFFIType]);
 
   final result = await callJsFunction(
     'fetch_url',
-    args: ['https://jsonplaceholder.typicode.com/todos/1'.toFFIType],
-  ).then((data) => JsonPayload(data: data).unpack());
+    args: ['https://jsonplaceholder.typicode.com/todos'.toFFIType],
+  ).then((data) => data.unpack());
   stdout.writeln(JsonEncoder.withIndent('  ').convert(result));
 
   exit(0);
--- examples/pretty_node/lib/pretty_node.dart
+++ examples/pretty_node/lib/pretty_node.dart
@@ -5,16 +5,15 @@ import 'dart:io';
 import 'package:globe_runtime/globe_runtime.dart';
 import 'package:pretty_node/pretty_node_source.dart';
 
-const moduleName = 'PrettyNode';
+const module = InlinedModule(name: 'PrettyNode', sourceCode: packageSource);
 
 Future<String> callJsFunction(
   String functionName, {
   List<FFIConvertible> args = const [],
 }) async {
   final completer = Completer<String>();
-  GlobeRuntime.instance.callFunction(
-    moduleName,
-    function: functionName,
+  module.callFunction(
+    functionName,
     args: args,
     onData: (data) {
       if (data.hasError()) {
@@ -29,7 +28,7 @@ Future<String> callJsFunction(
 }
 
 void main() async {
-  await GlobeRuntime.instance.registerModule(moduleName, packageSource);
+  await module.register();
 
   final prettyBytes = await callJsFunction('make_pretty_bytes', args: [
     569943344333884.toFFIType,
