38e75c65ec33d4f97d07ca9a13ef082aef0a5584
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-12-10 16:39:42 -0500

    feat: add additional init parameters

--- examples/pretty_node/lib/pretty_node.ts
+++ examples/pretty_node/lib/pretty_node.ts
@@ -2,8 +2,11 @@ import pretty_bytes from "pretty-bytes";
 import pretty_ms from "pretty-ms";
 import { defineSdk } from "@globe/runtime_types";
 
+type Language = "en" | "fr" | "es";
+
 type ModuleState = {
-  language: "en" | "fr" | "es";
+  language: Language;
+  verbose: boolean;
 };
 
 const make_pretty_bytes = <T = string>(
@@ -11,20 +14,20 @@ const make_pretty_bytes = <T = string>(
   value: number,
   callbackId: number
 ) => {
-  const byte_str = pretty_bytes(value);
+  const byte_str = pretty_bytes(value, { locale: state.language });
   const result = new TextEncoder().encode(byte_str);
   Dart.send_value(callbackId, result);
 };
 
 const make_pretty_ms = <T = string>(state: ModuleState, value: number, callbackId: number) => {
-  const byte_str = pretty_ms(value);
+  const byte_str = pretty_ms(value, { verbose: state.verbose });
   const result = new TextEncoder().encode(byte_str);
   Dart.send_value(callbackId, result);
 };
 
 export default defineSdk({
-  init(language: "en" | "fr" | "es" = "en"): ModuleState {
-    return { language };
+  init(language: Language = "en", verbose: boolean = false): ModuleState {
+    return { language, verbose };
   },
   functions: {
     make_pretty_bytes,
